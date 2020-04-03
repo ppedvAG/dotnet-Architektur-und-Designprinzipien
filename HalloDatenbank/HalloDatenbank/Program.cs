d9d82512663fc7dc4a4bdcdfe2f04e995257d710
André <ruland@outlook.de>
2020-04-03 16:24:10 +0200

    Datenbanken SQL/noSQL

---
+++ HalloDatenbank/HalloDatenbank/Program.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloDatenbank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conString = "Server=(localdb)\\mssqllocaldb;Database=Northwind;Trusted_Connection=true;";
+
+            var con = new SqlConnection(conString);
+            con.Open();
+            Console.WriteLine("DB Verbindung hergestellt");
+
+            var cmdCount = new SqlCommand();
+            cmdCount.Connection = con;
+            cmdCount.CommandText = "SELECT Count(*) FROM Employees";
+            var count = cmdCount.ExecuteScalar();
+            Console.WriteLine($"{count} Employees in DB");
+
+            var cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT * FROM Employees ORDER BY BirthDate";
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string vorname = reader["FirstName"].ToString();
+                string nachname = reader.GetString(1);
+                DateTime bDate = reader.GetDateTime(reader.GetOrdinal("BirthDate"));
+                Console.WriteLine($"{vorname} {nachname} ({bDate:d})");
+            }
+
+            var mySqlCon = new MySqlConnection("server ==...");
+
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HalloNoSQL/HalloNoSQL/Program.cs
@@ -0,0 +1,38 @@
+﻿using LiteDB;
+using System;
+
+namespace HalloNoSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using (var db = new LiteDatabase(@"C:\temp\MyData.db"))
+            {
+                var col = db.GetCollection<Auto>("Autos");
+
+                //    foreach (var item in col.Query().ToList())
+                //    {
+                //        Console.WriteLine(item.Hersteller);
+                //    }
+                col.Insert(new Auto() { Farbe = "blau", Baujahr = DateTime.Now.AddDays(-1047) });
+
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Auto
+    {
+
+        public int Id { get; set; }
+
+        public string Farbe { get; set; }
+        //public string Hersteller { get; set; }
+        public DateTime Baujahr { get; set; }
+    }
+}
